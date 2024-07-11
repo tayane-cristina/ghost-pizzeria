bffb28798a55fa25e8bcedb5bbe5d5a5dc31ebb1
Tayane Cristina <tay-cristinarj@hotmail.com>
2024-07-11 18:52:34 -0300

    tryng to  create the logic to handle with itens

--- ghost-pizzeria/src/components/cart/Cart.js
+++ ghost-pizzeria/src/components/cart/Cart.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Cart.css'
 import { useState } from 'react';
+import ReducerItens from '../reducerItens/ReducerItens';
+import { menu } from '../../data/Data';
 
 //Opção de desmarcar todos os itens
 //Soma de todos os itens dentro do carrinho no aside
@@ -14,84 +16,15 @@ descrição
 
 const Cart = () => {
 
-    const [cartList, setCartList] = useState([
-      {  id: 13,
-        displayName: "Alho e Óleo", 
-        rate: 3.5,
-        description: "alho, óleo, mussarela",
-        price: [
-          {P: 20,
-           M: 35,
-           G: 50,   
-          }
-        ],
-        isGourmet: false,
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpHDoBkPOw2-S3gTzkCZwIJhqHLpTdRRXpmA&s",
-      },
-      {  id: 14,
-        displayName: "Palmito", 
-        rate: 3.5,
-        description: "palmito, mussarela",
-        price: [
-          {P: 20,
-           M: 35,
-           G: 50,   
-          }
-        ],
-        isGourmet: false,
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1vvZtObGZ-SoWd7eu6G8nYUpBbb-_chWW-Q&s",
-      },
-    ])
-
-    const [totalPrice, setTotalPrice] = useState(0)
-
-    const emptyCart = () => {
-        <div>
-            <p><strong>Seu Carrinho está vazio</strong></p>
-            <img className='cart-emppty' src='https://img.icons8.com/?size=80&id=40LTSjteI583&format=png' alt='imagem-carrinho-vazio'/>
-        </div>
-    };
-
-    if (cartList.length <= 0) {
-      console.log("não tem nada dentro da lista")
-    } else {
-      console.log("Tem algo dentro da lista")
-    }
-
   return (
     <div className='cart-principal-div'>
       <div>
         <h2>Carrinho de compras</h2>
         <hr />
-      </div>
 
-      <div className='cart-totalPrice'>
-        <p><strong>Preço Total - R$ {totalPrice.toFixed(2)}</strong></p>
-        <button className='cart-btn-buy'>Finalizar a compra</button>
+        <ReducerItens productsList={menu}/>
+        
       </div>
-
-      {cartList.length <= 0 ? <p><strong>Seu carrinho está vazio</strong></p> : 
-      <ul className='cart-ul'>
-        {cartList.map((item, index) => (
-            <li key={index} className='cart-li'>
-                <img className='cart-img' src={item.image} alt={`image-${item.displayName}`}></img>
-                <section className='cart-infor'>
-                  <p><strong>{item.displayName}</strong></p>
-                  <p>{item.description}</p>
-                  
-                  <p>Selecione o tamanho</p>
-                  <form className='cart-form-option'>
-                    <input type="radio" id="TAM-P" name="TAM" value="P"></input>
-                    <label for="TAM-P">Pequena - R$20,00 | </label><br></br>
-                    <input type="radio" id="TAM-M" name="TAM" value="M"></input>
-                    <label for="TAM-M">Média - R$35,00 | </label><br></br>
-                    <input type="radio" id="TAM-G" name="TAM" value="G"></input>
-                    <label for="TAM-G">Grande - R$50,00</label><br></br>
-                  </form>
-                </section>       
-            </li>
-        ))}
-    </ul>}
     </div>
   );
 };
--- ghost-pizzeria/src/components/reducerItens/ReducerItens.js
+++ ghost-pizzeria/src/components/reducerItens/ReducerItens.js
@@ -1,17 +1,74 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useReducer} from 'react';
 
 const ReducerItens = ({productsList}) => {
 
-    const [products, setProducts] = useState([productsList])
+    const [products, setProducts] = useState(["pêra", "uva", "maça"])
 
     const initialState = {
         products,
         cartProducts: []
     }
 
+    const itensReducer = (state, action) => {
+      switch (action.type){
+
+        case "ADD":
+          return {
+            ...state,
+            cartProducts: []
+          }
+
+        case "REMOVE":
+          console.log("Você clicou no botão de remover do carrinho");
+
+        case "INCREASE_QUANTITY":
+          console.log("Você clicou no botão de aumentar a quantidade");
+        
+        case "DECREASE_QUANTITY":
+          console.log("Você clicou no botão de diminuir a quantidade");
+
+        default:
+          return state
+
+      };
+    };
+
+    const [state, dispatch] = useReducer(itensReducer, initialState);
+
+    const handleToAddItens = (item) => {
+      dispatch({type: "ADD"})
+    };
+
+    const handleToRemoveItens = (index) => {
+      dispatch({type: "REMOVE"})
+    };
+
+    const handleToIncreaseItens = () => {
+      dispatch({type: "INCREASE_QUANTITY"})
+    };
+
+    const handleToDeacreaseItens = () => {
+      dispatch({type: "DECREASE_QUANTITY"})
+    };
+
   return (
     <div>
+      <h2>Buttons</h2>
+
+    <ul>
+      {products.map((item, index) => (
+        <li key={index}>
+          <p>Nome: {item}</p>
+          <button onClick={() => handleToAddItens(item)}>button to add</button>
+          <button onClick={() => handleToRemoveItens(index)}>button to remove</button>
+          <button onClick={handleToIncreaseItens}>button to increase itens</button>
+          <button onClick={handleToDeacreaseItens}>button to decrease itens</button>
+        </li>
+      ))}
+  </ul>
+
+      
       
     </div>
   );
