67711f68d4c0ad2561b8cb644d9813fbbe9694f7
Tayane Cristina <tay-cristinarj@hotmail.com>
2024-08-05 14:04:29 -0300

    the componets about login and register was removed

--- ghost-pizzeria/src/components/navbar/Navbar.js
+++ ghost-pizzeria/src/components/navbar/Navbar.js
@@ -9,8 +9,6 @@ const Navbar = () => {
             <ul className='headerPage-pre-ul-authentication'>
                 <Link to='/'><span>GHOST Pizzeria</span></Link>
                 <Link to='/cart'><li><img src='https://img.icons8.com/?size=30&id=59997&format=png' alt='imagem-de-carrinho-de-compras'/></li></Link>
-                <li>Login</li>
-                <li>Criar cadastro</li>
             </ul>
             <ul className='headerPage-ul-SocialMedial'>
                 <li><img className='headerPage-ul-SocialMedial-img' src='https://img.icons8.com/?size=50&id=16733&format=png' alt='image-whatsApp'/></li>
